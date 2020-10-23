f1e477e547eb02c70c6f57dc3d101e77c4a065da
ricardodeveloper <ricardodeveloper21br@gmail.com>
2020-10-23 10:59:59 -0300

    ajuste plano de fundo

--- front/front-gestaoagrotech/src/app/app.module.ts
+++ front/front-gestaoagrotech/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MatSliderModule} from '@angular/material/slider';
 import {MatInputModule} from '@angular/material/input';
@@ -13,6 +14,7 @@ import {MatCardModule} from '@angular/material/card';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatSliderModule,
     MatInputModule,
     MatButtonModule,
